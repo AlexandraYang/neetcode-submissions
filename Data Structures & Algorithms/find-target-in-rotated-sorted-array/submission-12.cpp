474a42ac5a0ec788ee7a7d8c966aae1d5f3fd7f3
AlexandraYang <32972440+AlexandraYang@users.noreply.github.com>
2026-04-28 23:50:17 +0800

    Add: find-target-in-rotated-sorted-array - submission-12

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-12.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // 3,4,5,6,1,2
+        int l = 0;
+        int r = nums.size() -1;
+        int mid = 0;
+        while (l <= r) {
+            mid = l + (r-l)/2;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // 1,2,3,4,5,6
+            //part 1: 找 右半還是左半是 sorted 
+            if (nums[mid] <= nums[r]) { //這裡右半是sorted 
+                
+                // nums[mid] < target <= nums[r]
+                // 意思：target 比 mid 大 && target 沒超過 r
+
+
+               if (nums[mid] < target && target <= nums[r]  ) {
+                    // 1. 右半是 sorted
+                    // 2. target 在這個 sorted range
+                    // 3. target 在一個已排序區間內，而且在 mid 右邊
+                    // 4. mid 左邊全部不用看
+                    // 5. mid 自己也不用看
+                    l = mid + 1;
+                } else {
+                    // 1. 右半是 sorted
+                    // 2. target 不在這個 sorted range
+                    // 3. target 一定在另一半（左半）
+                    r = mid - 1;
+                }
+            } else {
+                // 左半sorted
+                if (nums[l] <= target && target < nums[mid]) {
+                    // 1. target 真的在第一層的左半
+                    // 2. 那就不用管右邊了 
+                    r = mid - 1;
+                } else {
+                    // 1. 左半是sorted 
+                    // 2. target 不在這 左半 [l, mid) 所以踢去第一層的右半去
+                    l = mid + 1;
+                }
+                
+                
+            }
+        }
+
+        return -1;
+    }
+};
