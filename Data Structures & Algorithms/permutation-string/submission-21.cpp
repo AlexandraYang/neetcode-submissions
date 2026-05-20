a4f99c75b2e6ab291ac1e06b6761de9742208073
AlexandraYang <32972440+AlexandraYang@users.noreply.github.com>
2026-05-20 09:01:38 +0800

    Add: permutation-string - submission-21

---
+++ Data Structures & Algorithms/permutation-string/submission-21.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.size() > s2.size()) {
+            return false;
+        }
+
+        vector<int> need(26, 0);
+        vector<int> windows(26, 0);
+        for (int i = 0; i < s1.size(); ++i) {
+            need[s1[i] - 'a']++;
+        }
+        int l =0;
+        for (int r = 0; r < s2.size(); ++r) {
+            windows[s2[r]-'a']++;
+
+            if (r-l+1 > s1.size()) {
+                windows[s2[l]-'a']--;
+                l++;
+            }
+
+            if (windows == need) {
+                return true;
+            }
+
+        }
+
+        return false;
+
+
+    }
+};
