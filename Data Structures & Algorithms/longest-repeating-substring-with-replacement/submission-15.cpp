3f349f277b2c1e13a1451903feee1ad71e6c098f
AlexandraYang <32972440+AlexandraYang@users.noreply.github.com>
2026-05-21 08:47:46 +0800

    Add: longest-repeating-substring-with-replacement - submission-15

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-15.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        //重點是 最大頻率要維護
+        int max_freq = 0;
+        int l = 0;
+        int ans = 0;
+
+        vector<int> count(26, 0);
+        for (int r =0; r < s.size(); ++r) {
+            count[s[r]-'A']++;
+            max_freq = max(max_freq, count[s[r]-'A']);
+
+            while((r-l+1) - max_freq > k ) {
+                //假設長度減掉最大頻率數字 大於可替換的val
+                //就要開始縮編 
+                count[s[l]-'A']--;
+                l++;
+            }
+
+            ans = max(ans, (r-l+1));
+        }
+
+        return ans;
+    }
+};
