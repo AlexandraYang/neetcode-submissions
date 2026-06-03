e8d325aff30986469be3cb0308c2d769dc6f3576
AlexandraYang <32972440+AlexandraYang@users.noreply.github.com>
2026-06-03 09:05:51 +0800

    Add: binary-tree-right-side-view - submission-9

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-9.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root) {
+            return {};
+        }
+
+        queue<TreeNode *> q;
+        vector<int> ans;
+        q.push(root);
+
+        while(!q.empty()) {
+            int sz = q.size();
+
+            for (int i = 0; i < sz; ++i) {
+                
+                TreeNode * tmp = q.front();
+                q.pop();
+
+
+                // keep the last one a.k.a the right most 
+                if (i == sz - 1) {
+                    ans.push_back(tmp->val);
+                }
+                
+
+                if (tmp->left) q.push(tmp->left);
+                if (tmp->right) q.push(tmp->right);
+
+            }
+
+        }
+
+        return ans;
+    }
+};
