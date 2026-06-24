535f85552affb8d399f92d05c30e4d4e654ddefa
AlexandraYang <32972440+AlexandraYang@users.noreply.github.com>
2026-06-24 08:57:51 +0800

    Add: serialize-and-deserialize-binary-tree - submission-2

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-2.cpp
@@ -0,0 +1,113 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Codec {
+private: 
+    vector<string> vals;
+
+    void build_str(TreeNode* node) {
+        if (!node) {
+            //假設為空 要方便字串辨識所以用 # 
+            vals.push_back("#");
+            return;
+        }
+        //中 
+        vals.push_back(to_string(node->val));
+
+        //左 
+        build_str(node->left);
+        //右 
+        build_str(node->right);
+        
+    }
+
+    TreeNode* dfs(vector<string>&vals, int &idx) {
+        if (vals[idx] == "#") {
+            idx++;
+            return nullptr;
+        }
+
+        TreeNode* node = new TreeNode(stoi(vals[idx]));
+        idx++;
+
+        node->left = dfs(vals, idx);
+        node->right = dfs(vals, idx);
+
+        return node;
+    }
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        vals.clear();
+        //step1: 先build 
+        build_str(root);
+
+        // step 2: 把build完的轉string  
+        // vector -> string
+        string res;
+        for (int i = 0; i < vals.size(); ++i) {
+            res += vals[i];
+            res += ",";
+        }
+
+        return res;
+    }
+
+    // Decodes your encoded data to tree.
+    //string → must convert to tokens first
+    TreeNode* deserialize(string data) {
+        if (data.empty()) {
+            return nullptr;
+        }
+        
+        // string -> vector 
+        vector<string> vals;
+        string cur;
+
+        for (char c: data) {
+            if (c == ',') {
+                vals.push_back(cur);
+                cur.clear(); // handle multi-digits 
+            } else {
+                cur.push_back(c);
+            }
+        }
+
+        
+        //如果 input 沒有最後逗號，最後一筆可能不見 
+        if (!cur.empty()) {
+            vals.push_back(cur);
+        }
+
+        int idx = 0;
+        return dfs(vals, idx);
+        
+
+
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
