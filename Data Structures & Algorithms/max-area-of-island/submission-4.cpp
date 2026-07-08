ad907e1ac37481495cb1a551611cd567a1b60ff3
AlexandraYang <32972440+AlexandraYang@users.noreply.github.com>
2026-07-09 00:27:32 +0800

    Add: max-area-of-island - submission-4

---
+++ Data Structures & Algorithms/max-area-of-island/submission-4.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private: 
+    int ans = 0;
+    int dfs(int i, int j, int m, int n, vector<vector<int>>& grid) {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0) {
+            return 0;
+        }
+
+        int cur = 1;
+        if (grid[i][j] == 1) {
+            grid[i][j] = 0;
+
+            
+        }
+
+        cur += dfs(i+1, j, m, n, grid);
+        cur += dfs(i-1, j, m, n, grid);
+        cur += dfs(i, j+1, m, n, grid);
+        cur += dfs(i, j-1, m, n, grid);
+        return cur;
+    }
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) {
+                    ans = max(ans, dfs(i, j, m,n,grid));
+
+                }   
+                
+            }
+        }
+
+        return ans;
+    }
+};
